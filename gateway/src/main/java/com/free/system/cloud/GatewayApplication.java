514885440ce02c12fc0edd1da111bcae1042e84a
suveng <suveng@163.com>
2019-05-24 12:40:55 +0800

    gateway zuul

---
+++ gateway/src/main/java/com/free/system/cloud/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.free.system.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * description:
+ * @author suwenguang@52tt.com
+ * @date 2019/5/23
+ * @version 1.0.0
+ **/
+@EnableZuulProxy
+@SpringCloudApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        System.out.println("########服务网关启动!########");
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
