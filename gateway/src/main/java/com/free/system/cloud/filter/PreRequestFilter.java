f1aec77a68d4df34001e60468b26435287e5a5da
suveng <suveng@163.com>
2019-05-24 12:47:17 +0800

    一些todo

---
+++ gateway/src/main/java/com/free/system/cloud/filter/PreRequestFilter.java
@@ -0,0 +1,40 @@
+package com.free.system.cloud.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * description:
+ * @author suwenguang@52tt.com
+ * @date 2019/5/24
+ * @version 1.0.0
+ **/
+@Component
+@Slf4j
+public class PreRequestFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        //todo:获取时间,真实ip,uri
+        //todo:鉴权
+        return null;
+    }
+}
