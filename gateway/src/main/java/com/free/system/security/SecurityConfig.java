80bbcc2a4080854c327856df87944fda97ca7d9b
suveng <suveng@163.com>
2019-06-08 15:25:23 +0800

    自定义登录方法,登录认证成功处理器

--- gateway/src/main/java/com/free/system/security/SecurityConfig.java
+++ gateway/src/main/java/com/free/system/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.free.system.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,7 +18,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-
+    @Autowired
+    private SystemAuthenticationSuccessHandler systemAuthenticationSuccessHandler;
     /**
      * 说明: 重写security的配置方法
      * @author suwenguang
@@ -28,9 +30,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //配置
         http.formLogin()
+                //自定义登录方法--REST 不需要
+                .loginProcessingUrl("")
+                .loginPage("/login")
+                //自定义登录认证成功处理器
+                .successHandler(systemAuthenticationSuccessHandler)
                 .and()
                 .authorizeRequests()
-                .anyRequest()
-                .authenticated();
+                //配置不需要认证的路由
+                //.antMatchers("**login","**static").permitAll()
+                //所有路由都要认证
+                .anyRequest().authenticated()
+                //禁用跨站攻击防护机制
+                .and().csrf().disable()
+                ;
     }
 }
