1d946f50a6b0dac55111b2841e1c1b2ac96b995c
suveng <suveng@163.com>
2019-07-20 23:31:30 +0800

    添加格式化文件

--- base/log/src/main/java/com/free/system/log/ILogDetail.java
+++ base/log/src/main/java/com/free/system/log/ILogDetail.java
@@ -7,23 +7,23 @@ package com.free.system.log;
  * @version 1.0.0
  **/
 public interface ILogDetail {
-    @Override
-    String toString();
+	@Override
+	String toString();
 
-    /**
-     * 说明: 获取接口返回的消息
-     * @author suwenguang
-     * @date 2019/7/16
-     * @return java.lang.String <- 返回类型
-     */
-    String getRes();
+	/**
+	 * 说明: 获取接口返回的消息
+	 * @author suwenguang
+	 * @date 2019/7/16
+	 * @return java.lang.String <- 返回类型
+	 */
+	String getRes();
 
-    /**
-     * 说明: 设置信息
-     * @author suwenguang
-     * @date 2019/7/16
-     * @return void <- 返回类型
-     */
-    void setMsg(String format, Object... arg);
+	/**
+	 * 说明: 设置信息
+	 * @author suwenguang
+	 * @date 2019/7/16
+	 * @return void <- 返回类型
+	 */
+	void setMsg(String format, Object... arg);
 
 }
--- gateway/src/main/java/com/free/system/GatewayApplication.java
+++ gateway/src/main/java/com/free/system/GatewayApplication.java
@@ -13,8 +13,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @SpringCloudApplication
 public class GatewayApplication {
-    public static void main(String[] args) {
-        System.out.println("########服务网关启动!########");
-        SpringApplication.run(GatewayApplication.class, args);
-    }
+	public static void main(String[] args) {
+		System.out.println("########服务网关启动!########");
+		SpringApplication.run(GatewayApplication.class, args);
+	}
 }
