06ca1ec7e737fc930bc201ef432352031bfedeff
suveng <suveng@163.com>
2019-07-31 18:38:00 +0800

    order 日志logfile开启

--- service/order/src/main/java/com/free/system/order/controller/OrderController.java
+++ service/order/src/main/java/com/free/system/order/controller/OrderController.java
@@ -4,8 +4,11 @@ import com.free.system.common.response.Response;
 import com.free.system.common.response.ResponseBuilder;
 
 import com.free.system.common.response.ResponseEnums;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * description:
@@ -15,9 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/order")
+@Slf4j
 public class OrderController {
 	@RequestMapping("/get")
-	public Response get() {
+	public Response get(HttpServletRequest request) {
+		log.info("用户访问了: "+request.getLocalAddr()+":"+request.getLocalPort());
 		return ResponseBuilder.build(ResponseEnums.SIMPLE_SUCCESS, "this is order service");
 	}
 }
