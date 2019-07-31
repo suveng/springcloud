3c131b398675e757b07da025ab03ef4c54d719e1
suveng <suveng@163.com>
2019-07-31 10:54:07 +0800

    增加spring boot admin 监控

---
+++ spring-boot-admin-server/src/main/java/com/free/system/AdminServerApplication.java
@@ -0,0 +1,52 @@
+package com.free.system;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * description:
+ * @author suwenguang
+ * @date 2019/5/23
+ * @version 1.0.0
+ **/
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class AdminServerApplication {
+	public static void main(String[] args) {
+		System.out.println("########监控中心启动!########");
+		SpringApplication.run(AdminServerApplication.class, args);
+	}
+
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters() {
+		//1、定义一个convert转换消息的对象
+		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+		//2、添加fastjson的配置信息
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		//3、在convert中添加配置信息
+		fastConverter.setFastJsonConfig(fastJsonConfig);
+		//4、将convert添加到converters中
+		HttpMessageConverter<?> converter = fastConverter;
+		// 解决乱码的问题
+		List<MediaType> fastMediaTypes = new ArrayList<>();
+		fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+		fastConverter.setSupportedMediaTypes(fastMediaTypes);
+		return new HttpMessageConverters(converter);
+	}
+}
