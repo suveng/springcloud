070aa26f7e6d9c6d810a5d032e57d7008271953d
suveng <suveng@163.com>
2019-08-11 14:08:17 +0800

    更新update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,10 @@ git push origin master
 git push github master
 git push gitlab master
 
+git push origin develop
+git push github develop
+git push gitlab develop
+
 git push --tags origin
 git push --tags github
 git push --tags gitlab
