a6f21571ebbf2a322c3dac6e25f8d9cb20525b8c
suveng <suveng@163.com>
2019-08-16 14:44:35 +0800

    更新架构

--- update.sh
+++ update.sh
@@ -9,12 +9,12 @@ git checkout develop
 
 git push origin master
 git push github master
-git push gitlab master
+git push gitee master
 
 git push origin develop
 git push github develop
-git push gitlab develop
+git push gitee develop
 
 git push --tags origin
 git push --tags github
-git push --tags gitlab
+git push --tags gitee
