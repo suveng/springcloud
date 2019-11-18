84dc9016525e65454ea94a914af81faf56e96ad6
suveng <suveng@163.com>
2019-11-18 16:43:20 +0800

    修改配置

---
+++ base/currency.result/src/main/java/com/qw/support/currency/result/model/database/BaseDO.java
@@ -0,0 +1,9 @@
+package com.qw.support.currency.result.model.database;
+
+/**
+ * database object 数据库基类
+ * @author suwenguang
+ * @version 1.0.0
+ **/
+public interface BaseDO {
+}
---
+++ base/currency.result/src/main/java/com/qw/support/currency/result/model/vo/BaseVO.java
@@ -0,0 +1,9 @@
+package com.qw.support.currency.result.model.vo;
+
+/**
+ * 视图渲染的数据对象
+ * @author suwenguang
+ * @version 1.0.0
+ **/
+public interface BaseVO {
+}
