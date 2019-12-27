419bc9f04d182d9a490a455f25e61c00d0af5a21
suveng <suveng@163.com>
2019-12-27 17:25:42 +0800

    user-provider

---
+++ 2.provider-user/src/test/java/qw/test/user/ApplicationTests.java
@@ -0,0 +1,13 @@
+package qw.test.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
